44c6d6d7280032044f9fb8f0ea870cecbbb71479
Cgg <clement.geiger@gmail.com>
2011-08-23 22:28:49 +0100

    ...

--- InvertedPendulum/RKIP/InvertedPendulum.m
+++ InvertedPendulum/RKIP/InvertedPendulum.m
@@ -7,6 +7,7 @@
 %
 % Input arguments :
 % ---------------
+% - iA   : initial value for pendulum's angle
 % - l    : pendulum's length
 % - m    : pendulum's weight
 % - M    : cart's weight
@@ -26,6 +27,48 @@
 % - all speeds and accelerations are null
 % - simulation's length becomes greater than T (cf. input args)
 
-function [ T, x, sp, ac, a, asp, aac ] = InvertedPendulum( l, m, M, f, dt, TMax )
+function [ T, x, sp, ac, a, asp, aac ] = InvertedPendulum( iA, l, m, M, f, dt, TMax )
+
+% tolerance over stop condition. Should be input arg.
+eps = 1e-3;
+
+nStep = TMax / dt;
+
+T   = zeros( nStep, 1 );
+x   = zeros( nStep, 1 );
+sp  = zeros( nStep, 1 );
+ac  = zeros( nStep, 1 );
+a   = zeros( nStep, 1 );
+asp = zeros( nStep, 1 );
+aac = zeros( nStep, 1 );
+
+a( 1 ) = iA;
+
+% Runge-Kutta methode is used 4 times here.
+kSp  = zeros( 4, 1 );
+kAc  = zeros( 4, 1 );
+kASp = zeros( 4, 1 );
+kAAc = zeros( 4, 1 );
+
+i = 2;
+
+runningPendulum = 1;
+
+% Computing results
+% i refers to the currently computed position/speed/angle/etc.
+% ( i - 1 ) refers to the previous one
+while( ( i < nStep ) & ( runningPendulum == 1 ) )
+
+
+
+    % Checking the other stop condition
+    if( abs( sp( i ) ) <= eps &
+        abs( ac( i ) ) <= eps &
+        abs( asp( i ) ) <= eps &
+        abs( aac( i ) ) <= eps )
+
+        runningPendulum = 0;
+    end
+end
 
 end
